83a6839c39b3fac5b24e60f1ab775c3f33d9ed18
tonymarkel <32904980+tonymarkel@users.noreply.github.com>
2023-01-28 08:31:48 -0500

    Version 0.1.2
    Adds Readme, pretty print of results.

--- func.py
+++ func.py
@@ -1,5 +1,5 @@
 #
-# oci-kms-swap version 1.0.
+# ociswapkms
 #
 # Copyright (c) 2023 Oracle, Inc.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
@@ -16,7 +16,7 @@ def handler(ctx, data: io.BytesIO=None):
     resp = swap_kms_instances(ctx, signer)
     return response.Response(
         ctx,
-        response_data=json.dumps(resp),
+        response_data=json.dumps(resp, indent = 2),
         headers={"Content-Type": "application/json"}
     )
 
@@ -24,7 +24,7 @@ def handler(ctx, data: io.BytesIO=None):
 def swap_kms_instances(ctx, signer):
     cfg = ctx.Config()
     key = cfg["kmskeyid"]
-    status = ""
+    status = []
     client = oci.core.BlockstorageClient(config={}, signer=signer)
     # Applies Customer Managed Keys to new compute instances
     try:
@@ -32,7 +32,7 @@ def swap_kms_instances(ctx, signer):
         current_boot_volumes = client.list_boot_volumes(compartment_id=signer.compartment_id)
         for v in current_boot_volumes.data:
             if v.kms_key_id == key:
-                print(v.id, "Correct Key Present")
+                status.append({'volume':v.id,'key':v.kms_key_id,'status':'Unchanged','Compliant':True,'Lifecycle State':v.lifecycle_state})
             else:
                 if v.lifecycle_state == "AVAILABLE":
                     client.update_boot_volume_kms_key(
@@ -41,14 +41,14 @@ def swap_kms_instances(ctx, signer):
                             kms_key_id="ocid1.key.oc1.iad.b5r33vmcaah4o.abuwcljtjfdyu6qomsh52acdusxxs5qqutmeyzsdxw2b4exzmnyif4r6jydq",
                         )
                     )
-                    status += "Key Applied for " + v.id + ".\n"
+                    status.append({'volume':v.id,'key':key,'status':'Changed','Compliant':True,'lifecycle state':v.lifecycle_state})
                 else:
-                    status += "This function not available for " + v.id + " because it is " + v.lifecycle_state + ".\n"
+                    status.append({'volume':v.id,'key':v.kms_key_id,'status':'Unchanged','Compliant':False,'Lifecycle State':v.lifecycle_state})
         # Applies keys to the boot volumes in the current compartment
         current_block_volumes = client.list_volumes(compartment_id=signer.compartment_id)
         for v in current_block_volumes.data:
             if v.kms_key_id == key:
-                print(v.id, "Correct Key Present")
+                status.append({'volume':v.id,'key':v.kms_key_id,'status':'Changed','Compliant':True,'lifecycle state':v.lifecycle_state})
             else:
                 if v.lifecycle_state == "AVAILABLE":
                     client.update_volume_kms_key(
@@ -57,11 +57,11 @@ def swap_kms_instances(ctx, signer):
                             kms_key_id="ocid1.key.oc1.iad.b5r33vmcaah4o.abuwcljtjfdyu6qomsh52acdusxxs5qqutmeyzsdxw2b4exzmnyif4r6jydq",
                         )
                     )
-                    status += "Key Applied for " + v.id + ".\n"
+                    status.append({'volume':v.id,'key':key,'status':'Changed','Compliant':True,'lifecycle state':v.lifecycle_state})
                 else:
-                    status += "Status not available for " + v.id + " because it is " + v.lifecycle_state + ".\n"
+                    status.append({'volume':v.id,'key':v.kms_key_id,'status':'Changed','Compliant':False,'lifecycle state':v.lifecycle_state})
     except Exception as ex:
         print("ERROR: accessing Block Volumes failed", ex, flush=True)
         raise
-    resp = { "Operation Complete" : status }
+    resp = { "result" : status }
     return resp
